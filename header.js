ad325c49b8c6a3143af30fe9738efaa14d790a9b
Josh Bivens <hotbioform@gmail.com>
2017-01-07 17:42:28 -0700

    Add selectors

---
+++ header.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const logo = document.querySelector('h1');
+const nav = document.querySelector('ul');
+const borders = document.querySelector('.borders');
+const border = [...document.querySelectorAll('.border')];
