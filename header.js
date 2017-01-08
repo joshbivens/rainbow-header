ef8532091f9773869487763075e95a904172f4c0
Josh Bivens <hotbioform@gmail.com>
2017-01-07 17:43:12 -0700

    Add shrink/grow to header

--- header.js
+++ header.js
@@ -4,3 +4,18 @@ const logo = document.querySelector('h1');
 const nav = document.querySelector('ul');
 const borders = document.querySelector('.borders');
 const border = [...document.querySelectorAll('.border')];
+
+window.onscroll = () => {
+  // Shrink Header/Borders
+  if(body.scrollTop > 100) {
+    header.classList.add('shrink');
+    logo.classList.add('shrink-logo');
+    nav.classList.add('shrink-nav');
+    borders.classList.add('shrink-borders');
+  } else {
+    header.classList.remove('shrink');
+    logo.classList.remove('shrink-logo');
+    nav.classList.remove('shrink-nav');
+    borders.classList.remove('shrink-borders');
+  }
+}
